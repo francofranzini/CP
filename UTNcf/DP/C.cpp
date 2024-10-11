705b0c5ce82c9d1b01985800f283bede272cf9ed
Franco Franzini <francofranzini12@gmail.com>
2024-10-10 21:52:34 -0300

    Corregidos nombres y eliminados archivos de prueba

---
+++ UTNcf/DP/C.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for(int i = 0; i<n; i++)
+
+int main(){
+    //ingreso n y k
+    //lista de n enteros cuya suma es alfa.k y el maximo elemento de la lista es el minimo
+    //posible
+    return 0;   
+}
