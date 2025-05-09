bb4a9af84f1a0200342d95d075ee93ee52ad86c7
Franco Franzini <francofranzini12@gmail.com>
2025-05-08 22:42:22 -0300

    ABURRIDO

--- Codeforces/Clases/Backtracking/NQueen.cpp
+++ Codeforces/Clases/Backtracking/NQueen.cpp
@@ -21,20 +21,46 @@ const int INF = 1e9;
 const ll LINF = 1e18;
 const int MOD = 1e9 + 7;
 
-vector<pii> backtrack(int n, vi columnas, vi d1, vi d2, int fila, vector<pii> tablero){
-	if(fila == n) return tablero;
+int sepuede(int n, si &columnas, si &d1, si &d2, int fila, int columna){
+	if(columnas.find(columna) != columnas.end()) return 0;
+	if(d1.find(fila+columna) != d1.end()) return 0;
+	if(d2.find(fila - columna) != d2.end()) return 0;
+	return 1;
 }
+void mostrar_tablero(vector<vi> tablero, int n){
+	forn(i, n){
+		forn(j, n){
+			if(tablero[i][j] == 1 ? cout << 'Q' : cout << ".");
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+void backtrack(int n, si &columnas, si &d1, si &d2, int fila, vector<vi> tablero){
+	if(fila == n) mostrar_tablero(tablero, n);
+	forn(i, n){
+		if(sepuede(n, columnas, d1, d2, fila, i)){
+			tablero[fila][i] = 1;
+			columnas.insert(i);
+			d1.insert(fila + i);
+			d2.insert(fila - i);
+			backtrack(n, columnas, d1, d2, fila+1, tablero);
+			tablero[fila][i] = 0;
+			columnas.erase(i);
+			d1.erase(fila+i);
+			d2.erase(fila - i);
+		}
+	}
+}
+
 
 void solve() {
 	int n;
 	cin >> n;
-	vector<vector<pii>> rta;
-	vector<pii> aux;
-	vi d1, d2, columnas;
-	forn(i, n){
-		aux = backtrack(n, columnas, d1, d2, i)
-	}
-
+	vector<vector<int>> a(n, vector<int>(n, 0));
+	si d1, d2, columnas;
+	backtrack(n, columnas, d1, d2, 0, a);
 }
 
 int main() {
